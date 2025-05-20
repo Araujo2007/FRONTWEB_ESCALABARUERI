06c3a4bc5fb89d383ad8e5e41deabffd7301041b
Vinicius <vinicius.jesus49@etec.sp.gov.br>
2025-05-20 10:48:07 -0300

    Atualização Font Web

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import login from "./pages/login"
 
-
 function App() {
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" Component={login}/>
-    </Routes>
+      <Routes>
+        <Route path="/" Component={login} />
+      </Routes>
     </BrowserRouter>
   )
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import '../../style/login.css'
+import Logo from "../../assets/logo.png"
+
 // import styles from './index.module.css'
 
-function login(){
+function login() {
 
-    return(
+    return (
         // <div className={styles.div}>
         <div>
             <div className="container">
-            <div className="areaLogin">
-                <div className="areaLogo">
-                    <img src="" alt="" />
-                    <h1>Escala Barueri</h1>
-                </div>
-                <div className="areaPreenchimento">
-                    <h1>Administrador</h1>
-                    <br />
-                    <h2>Login</h2>
-                    <br />
-                    <input type="number" placeholder="Número de Matrícula" />
+                <div className="areaLogin">
+                    <div className="areaLogo">
+                        <div className="elementosLogo">
+                            <img src={Logo} alt="" />
+                            <h1 className="tituloLogo">Escala</h1>
+                            <h1 className="tituloLogo">Barueri</h1>
+                        </div>
+
+                    </div>
+                    <div className="areaPreenchimento">
+                        <h1>Administrador</h1>
+                        <br />
+                        <h2>Login</h2>
+                        <br />
+                        <input type="text" placeholder="Número de Matrícula" />
+                        <br /><br />
+                        <input type="password" placeholder="Senha" />
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 
