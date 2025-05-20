461e17a56fd3e3dab9cc3bb96385e9a391227e3c
Vinicius <vinicius.jesus49@etec.sp.gov.br>
2025-05-20 08:48:55 -0300

    Atualização Front

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import '../../style/login.css'
+// import styles from './index.module.css'
 
 function login(){
 
     return(
+        // <div className={styles.div}>
         <div>
-            <h1>jglfdshljkdnjhglfdjdghjkg</h1>
+            <div className="container">
+            <div className="areaLogin">
+                <div className="areaLogo">
+                    <img src="" alt="" />
+                    <h1>Escala Barueri</h1>
+                </div>
+                <div className="areaPreenchimento">
+                    <h1>Administrador</h1>
+                    <br />
+                    <h2>Login</h2>
+                    <br />
+                    <input type="number" placeholder="Número de Matrícula" />
+                </div>
+            </div>
+        </div>
         </div>
     )
 
