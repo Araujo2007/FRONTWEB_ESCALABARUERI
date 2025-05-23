9d3577dc66043efa172b2927fd4682e0a0c6c2d3
Vinicius <vinicius.jesus49@etec.sp.gov.br>
2025-05-23 10:57:24 -0300

    atualização

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,49 +1,49 @@
 import React from "react";
-import '../../styles/login.css'
+import styles from '../../styles/login.module.css'
 import Logo from "../../assets/logo.png"
-import { useRef } from "react";
+import { useForm } from "react-hook-form"
 
 // import styles from './index.module.css'
 
 function login() {
-
-    const matricula = useRef()
-    const password = useRef()
-
-    function handleSubmit(event) {
-        event.preventDefault()
-
-        try {
-            matricula.current.value,
-            password.current.value
-        } catch (error) {
-            
-        }
+    const onSubmit = () => {
+        
     }
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+
+
     return (
+
         // <div className={styles.div}>
         <div>
-            <div className="container">
-                <div className="areaLogin">
-                    <div className="areaLogo">
-                        <div className="elementosLogo">
+            <div className={styles.container}>
+                <div className={styles.areaLogin}>
+                    <div className={styles.areaLogo}>
+                        <div className={styles.elementosLogo}>
                             <img src={Logo} alt="" />
-                            <h1 className="tituloLogo">Escala</h1>
-                            <h1 className="tituloLogo">Barueri</h1>
+                            <h1 className={styles.tituloLogo}>Escala</h1>
+                            <h1 className={styles.tituloLogo}>Barueri</h1>
                         </div>
 
                     </div>
-                    <div className="areaPreenchimento">
-                        <form action="">
-                            <h1 className="textAdm">Administrador</h1>
+                    <div className={styles.areaPreenchimento}>
+                        <form  onSubmit={handleSubmit(onSubmit)}>
+                            <h1 className={styles.textAdm}>Administrador</h1>
                             <h2>Login</h2>
                             <br />
-                            <input type="text" placeholder=" Número de Matrícula" ref={matricula} />
+                            <input type="text" placeholder=" Número de Matrícula" {...register("matricula", {
+                                required: true,
+                                pattern: /^[0-9]+$/i
+                            })} />
                             <br /><br />
-                            <input type="password" placeholder=" Senha" ref={password} />
+                            <input type="password" placeholder=" Senha" {...register("password")} />
                             <br /><br />
-                            <input className="button" type="button" value="ENTRAR" />
+                            <input className="button" type="submit" value="ENTRAR" />
                             <br /><br />
                             <a href="">ESQUECI MINHA SENHA</a>
                         </form>
