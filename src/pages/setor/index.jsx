5ed11eddedff5a0bf5a55eb6454bec5a37355548
Vinicius <vinicius.jesus49@etec.sp.gov.br>
2025-05-27 15:10:13 -0300

    atualização

--- src/pages/setor/index.jsx
+++ src/pages/setor/index.jsx
@@ -1,31 +1,42 @@
-import React from "react"
-import styles from '../../styles/setor.module.css'
-import { useForm } from "react-hook-form"
-import { OffcanvasProvider, Trigger, Offcanvas } from 'react-simple-offcanvas'
-
-function setor() {
-    return (
-        <div className={styles.body}>
-            <OffcanvasProvider { /* Provider props */ }>
-                <Trigger { /* Trigger props */ } />
-                <Offcanvas { /* Offcanvas props */ } />
-            </OffcanvasProvider>
-            <div className={styles.container}>
-                <div className={styles.areaSetor}>
-                    <h1>Cadastro de Setor</h1>
-                    <form>
-                        <label htmlFor="setor">Setor</label>
-                        <input type="text" id="setor" name="setor" placeholder="Digite o nome do setor" />
-
-                        <label htmlFor="descricao">Descrição</label>
-                        <textarea id="descricao" name="descricao" placeholder="Digite a descrição do setor"></textarea>
-
-                        <button type="submit">Cadastrar</button>
-                    </form>
-                </div>
-            </div>
+import React, { useState } from 'react';
+import styles from '../../styles/setor.module.css';
+ 
+function Setor() {
+  const [menuAberto, setMenuAberto] = useState(false);
+ 
+  return (
+    <div className={styles.body}>
+      {/* Navbar */}
+      <nav className={styles.navbar}>
+        <button className={styles.menuButton} onClick={() => setMenuAberto(true)}>☰ Menu</button>
+        <h1 className={styles.logo}>Minha App</h1>
+      </nav>
+ 
+      {/* Offcanvas (menu lateral) */}
+      <div className={`${styles.offcanvas} ${menuAberto ? styles.ativo : ''}`}>
+        <button className={styles.fechar} onClick={() => setMenuAberto(false)}>×</button>
+        <ul className={styles.menuList}>
+          <li><a href="/">Início</a></li>
+          <li><a href="/setor">Setor</a></li>
+          <li><a href="/funcionario">Funcionários</a></li>
+        </ul>
+      </div>
+ 
+      {/* Conteúdo principal */}
+      <div className={styles.container}>
+        <div className={styles.areaSetor}>
+          <h1>Cadastro de Setor</h1>
+          <form>
+            <label htmlFor="setor">Setor</label>
+            <input type="text" id="setor" name="setor" placeholder="Digite o nome do setor" />
+            <label htmlFor="descricao">Descrição</label>
+            <textarea id="descricao" name="descricao" placeholder="Digite a descrição" />
+            <button type="submit">Cadastrar</button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-export default setor;
+ 
+export default Setor;
