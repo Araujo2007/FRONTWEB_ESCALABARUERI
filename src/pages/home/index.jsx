96e471e8bd90eeb57cdb79e8f0005d4fafe069ed
Vinicius <vinicius.jesus49@etec.sp.gov.br>
2025-06-03 15:11:26 -0300

    atygh

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,10 +4,12 @@ import Logo from "../../assets/icons/barueri.png"
 import exit from "../../assets/icons/exit.png"
 import notification from "../../assets/icons/notification.png"
 import lupa from "../../assets/icons/lupa.png"
+import dash from "../../assets/Frame 15.png"
 
 function home() {
     return (
         <div className={styles.container}>
+            <div className={styles.fixed}>
             <div className={styles.secretariaElogo}>
                 <div className={styles.textSecretaria}>
                     <h2 className={styles.h2Logo}>Secretaria de </h2>
@@ -22,13 +24,36 @@ function home() {
                 <p className={styles.boasVindas}>Bem Vindo(a) Rogério</p>
                 <img src={notification} alt="" className={styles.notification} />
             </nav>
+            </div>
             <br />
             <div className={styles.barraPesquisa}>
                 <input type="text" className={styles.inputPesquisa} placeholder='  Pesquisar... ' />
                 <div className={styles.lupa}>
                     <img className={styles.imgLupa} src={lupa} alt="" />
-                    </div>
-            </div>        
+                </div>
+            </div>
+            <br />
+            <div className={styles.dashTeste}><img src={dash} alt="" className={styles.dash}/></div>
+            <br />
+            <div className={styles.setoresEequipes}>
+                <div className={styles.setores}>
+                    <h2 className={styles.titulo}>Setores</h2>
+                    <button type="button" className={styles.setor}>Administração</button> 
+                    <button type="button" className={styles.setor}>Recursos Humanos</button> 
+                    <button type="button" className={styles.setor}>Suporte</button> 
+                    <button type="button" className={styles.setor}>Atendimento</button> 
+                    <button type="button" className={styles.setor}>+</button> 
+                    <h2 className={styles.titulo}>Equipes</h2>
+                    <button type="button"  className={styles.setor}>Alfa</button> 
+                    <button type="button" className={styles.setor}>Bravo</button> 
+                    <button type="button" className={styles.setor}>Charlie</button> 
+                    <button type="button" className={styles.setor}>Delta</button> 
+                </div>
+                <div className={styles.opcoes}>
+                    <button className={styles.setor}>Consultar Datas</button>
+                    
+                </div>
+            </div>
         </div>
     )
 }
